9913961b2bff7d93a54d67ca85256ec1a4ecebdb
Đỗ Long Ánh <98139573+Jangkuz@users.noreply.github.com>
2024-12-02 17:48:31 +0700

    fix: correct react-native ver for android

--- Twitter/app/(drawer)/_layout.tsx
+++ Twitter/app/(drawer)/_layout.tsx
@@ -2,6 +2,11 @@ import {Stack, withLayoutContext} from "expo-router"
 import {createDrawerNavigator} from '@react-navigation/drawer'
 
 const DrawerNavigator = createDrawerNavigator().Navigator;
+export const unstable_settings = {
+    // Ensure that reloading on `/modal` keeps a back button present.
+    // initialRouteName: "(tabs)",
+    initialRouteName: "(tabs)",
+};
 const Drawer = withLayoutContext(DrawerNavigator);
 export default function DrawerLayout(){
     return (<Drawer/>)
